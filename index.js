03430f8b43b22c86b53d55f3aec8fdb3b69a9988
Anil chouhan <anilchouhan1259@gmail.com>
2022-02-12 16:02:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+var buttonNumber = document.querySelectorAll(".drum").length;
+for (var i = 0; i < buttonNumber; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var innerbutton = this.innerHTML;
+    makesound(innerbutton);
+    buttonanimation(innerbutton);
+
+  });
+}
+document.addEventListener("keydown",function(event){
+  makesound(event.key);
+  buttonanimation(event.key);
+});
+function makesound(key){
+switch (key) {
+  case 'w':
+    var tom1 = new Audio('sounds/tom-1.mp3');
+    tom1.play();
+    break;
+  case 'a':
+    var tom2 = new Audio('sounds/tom-2.mp3');
+    tom2.play();
+    break;
+  case 'l':
+    var tom3 = new Audio('sounds/tom-3.mp3');
+    tom3.play();
+    break;
+  case 's':
+    var tom4 = new Audio('sounds/tom-4.mp3');
+    tom4.play();
+    break;
+  case 'd':
+    var snare = new Audio('sounds/snare.mp3');
+    snare.play();
+    break;
+  case 'j':
+    var kick = new Audio('sounds/kick-bass.mp3');
+    kick.play();
+    break;
+  case 'k':
+    var crash = new Audio('sounds/crash.mp3');
+    crash.play();
+    break;
+  default:
+    console.log(innerbutton);
+
+}
+}
+function buttonanimation(currentkey){
+var activebutton = document.querySelector("."+currentkey);
+activebutton.classList.add("pressed");
+setTimeout(function(){
+activebutton.classList.remove("pressed");
+}, 100);
+}
